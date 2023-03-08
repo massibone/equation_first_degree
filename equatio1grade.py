d9d67df37cb3b574255750320fb2deec771e2b5d
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-03-08 08:04:13 +0100

    Add files via upload

---
+++ equatio1grade.py
@@ -0,0 +1,9 @@
+def plug():
+    x = -100 #start at ­100
+    while x < 100: #go up to 100
+        if 2*x + 5 == 13: #if it makes the equation true
+            print("x =",x) #print it out
+        x += 1 #make x go up by 1 to test the next number
+
+
+plug() 
